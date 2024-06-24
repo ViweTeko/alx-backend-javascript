b86f98acd679231a9e6b344bad141daa7de1de8e
ViweTeko <iamviwe.teko@gmail.com>
2024-06-25 00:38:45 +0200

    Advanced

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIterator(report) {
+  const employees = Object.values(report.allEmployees).flat();
+  return {
+    *[Symbol.iterator]() {
+      for (const employee of employees) {
+        yield employee;
+      }
+    },
+  };
+}
