4b887b70e4f61efea5d23fbdb82cb4601e9feafd
ViweTeko <iamviwe.teko@gmail.com>
2024-06-25 11:14:02 +0200

    fixed error(s)

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -6,9 +6,9 @@ function getCurrentYear() {
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const year = getCurrentYear();
   const budget = {
-    [`income-${year()}`]: income,
-    [`gdp-${year()}`]: gdp,
-    [`capita-${year()}`]: capita,
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita,
   };
 
   return budget;
