5a5931baa4766071f4415162f8e527f5158d64d5
ViweTeko <iamviwe.teko@gmail.com>
2024-07-01 00:11:27 +0200

    Promise to redo

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
-import { uploadPhoto, createUser } from "../utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
       console.log(
-        `${photoResult.body} ${userResult.firstName} ${userResult.lastName}`
+        `${photoResult.body} ${userResult.firstName} ${userResult.lastName}`,
       );
     })
     .catch(() => {
-      console.log("Signup system offline");
+      console.log('Signup system offline');
     });
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,12 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises).then((values) => {
     const result = [];
     values.forEach((element) => {
-      if (element.status === "fulfilled") {
+      if (element.status === 'fulfilled') {
         result.push({
           status: element.status,
           value: element.value,
